9f14f97fe65cfd7a84e55535d5488b9ff5dfdb4b
KassianoV <kassianovieira.id.uff.br>
2022-01-21 18:50:17 -0300

    atualização item

--- ProdutoCarrinho.py
+++ ProdutoCarrinho.py
@@ -1,5 +1,31 @@
-from Produto import Produto
+from ProdutoEstoque import ProdutoEstoque
 
-class ProdutoCarrinho(Produto):                     
-    def __init__(self, nome, val, qtd):
-        super().__init__(nome, val, qtd)
+class ProdutoCarrinho():                     
+    nome: str
+    quantidade: int
+
+    def __init__(self, nome, qtd):
+        self.nome = nome
+        self.quantidade = qtd
+
+    def nomeProduto(self, nome):
+        self.nome = nome
+        print("Qual produto deseja:", nome)
+
+    def atualizaQuantidade(self, qtd):                 # atualizar a quantidade do "Produto" e printa na tela 
+        self.quantidade = qtd
+        print("nova quantidade:", qtd)
+
+    def setQuantidade(self, qtd):                                 #definir a quantidade 
+        self.quantidade = qtd
+
+    def setNome(self,nome):
+        self.nome = nome
+
+    def getQuantidade(self):                                        #pegar essa quantidade 
+        return self.quantidade
+
+    def getNome(self):                                                   #pegar esse nome
+        return self.nome
+
+
